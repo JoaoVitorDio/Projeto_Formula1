7af42b9ba91e93ffa6d56ebead3605ce09898ccf
João Vitor Diógenes <joaovitor.diogenes@hotmail.com>
2022-07-03 15:51:02 -0300

    Loading Users Table

--- CreateDatabaseAndLoad/CreateUsersTableAndLoad/CreateUsersTableAndLoad.py
+++ CreateDatabaseAndLoad/CreateUsersTableAndLoad/CreateUsersTableAndLoad.py
@@ -1,10 +1,10 @@
 import os
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 #Reading environment variables to get database name and password
-database_name = os.environ['DatabaseName']
-postgres_password = os.environ['PostgresPassword']
+database_name = os.environ["DatabaseName"]
+postgres_password = os.environ["PostgresPassword"]
 
 DATABASES = {
     'production':{
@@ -17,24 +17,29 @@ DATABASES = {
 }
 
 # choose the database to use
-db = DATABASES['production']
-
+db = DATABASES["production"]
 # construct an engine connection string
 engine_string = "postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}".format(
     user = db['USER'],
     password = db['PASSWORD'],
     host = db['HOST'],
     port = db['PORT'],
-    database = db['NAME'],
+    database = db['NAME']
 )
 
 # create sqlalchemy engine
 engine = create_engine(engine_string)
-
-# read a table from database into pandas dataframe, replace "tablename" with your table name
-
-#df = pd.read_sql_table('results',engine)
 conn = engine.connect()
 
 
-df = pd.read_sql_query('select * from RESULTS',conn)
+#Creating the systems admin
+sql_text = text("INSERT INTO USERS VALUES (DEFAULT,'admin','admin','Administrador',NULL)")
+result = conn.execute(sql_text)
+
+
+# Inserting drivers and constructors already signed into users table
+# read a table from database into pandas dataframe
+df = pd.read_sql_query("SELECT CONCAT(driverref,'_d') AS login, driverref as password FROM driver",conn)
+
+
+conn.close()
