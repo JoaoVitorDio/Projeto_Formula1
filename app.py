36732f28497318466228da1f09cd6626029a0505
Julio Casemiro <casemisto@gmail.com>
2022-07-03 18:41:31 -0300

    reformatted file, changed script to use dotenv files and add a dotenv sample

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import os
 import pandas as pd
+from flask.cli import load_dotenv
 from sqlalchemy import create_engine, text
 from flask import Flask, render_template, request
 
-#Reading environment variables to get database name and password
-database_name = os.environ['DatabaseName']
-postgres_password = os.environ['PostgresPassword']
+# Creating environment based on .env file
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
+# Reading environment variables to get database name and password
 DATABASES = {
-    'production':{
-        'NAME': database_name,
-        'USER': 'postgres',
-        'PASSWORD': postgres_password,
+    'production': {
+        'NAME': os.environ['DATABASE_NAME'],
+        'USER': os.environ['DATABASE_USERNAME'],
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
         'HOST': '127.0.0.1',
         'PORT': 5432,
     },
@@ -21,11 +23,11 @@ DATABASES = {
 db = DATABASES['production']
 # construct an engine connection string
 engine_string = "postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}".format(
-    user = db['USER'],
-    password = db['PASSWORD'],
-    host = db['HOST'],
-    port = db['PORT'],
-    database = db['NAME']
+    user=db['USER'],
+    password=db['PASSWORD'],
+    host=db['HOST'],
+    port=db['PORT'],
+    database=db['NAME']
 )
 
 # create sqlalchemy engine
@@ -34,25 +36,29 @@ conn = engine.connect()
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/submit', methods=['POST'])
 def submit():
     login = request.form['login']
     password = request.form['password']
-    
+
     # Inserting drivers already signed into users table
-    sqlQuery = f'''SELECT userid, login, tipo, idoriginal FROM users WHERE login = '{login}' and 
-                           password = MD5(CONCAT('{password}',userID))'''
-    df = pd.read_sql_query(sqlQuery,conn)
-    
-    if (len(df.index) > 0):
-        return render_template('success.html', login = login, password = password) 
+    sqlQuery = f'''
+    SELECT userid, login, tipo, idoriginal FROM users WHERE login = '{login}' and 
+                           password = MD5(CONCAT('{password}',userID))
+                           '''
+    df = pd.read_sql_query(sqlQuery, conn)
+
+    if len(df.index) > 0:
+        return render_template('success.html', login=login, password=password)
     else:
         return 'hm'
-    
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
